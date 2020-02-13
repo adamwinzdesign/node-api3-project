9ec2a04eefd656085cdda8130ba3ab8043f0dfe3
adamwinzdesign <adamwinzdesign@gmail.com>
2020-02-12 22:23:03 -0600

    building userRouter

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const helmet = require('helmet');
+
+const userRouter = require('./users/userRouter');
 
 const server = express();
 
+
 server.use(helmet());
-server.use(morgan('dev'))
-server.use(logger)
+// server.use(morgan('dev'))
+// server.use(morgan('[:date[web]] :method :url :status :response-time ms'));
+// server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -15,9 +19,13 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(`${req.method} Request to ${req.originalUrl} `);
+  const date = new Date();
+  console.log(`${req.method} Request to ${req.originalUrl} on ${date.toDateString()}`);
 
   next();
 }
+
+server.use(express.json());
+server.use('/api/users/', userRouter);
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
+const users = require('./userDb');
+
 const router = express.Router();
 
+// post new user
 router.post('/', (req, res) => {
-  // do your magic!
+  const name = req.body;
+  // console.log(name);
+  // console.log(req.body);
+  users.insert(name)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'Server error adding a new user', error })
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -33,15 +45,24 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  // validates the user id on every request that expects a user id parameter
+  // if id is valid, store that user object as req.user
+  // if id does not match any user id in the db, respond with 400 and { message: "invalid user id" } 
+  next(); 
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  // validates the body on a request to create a new user
+  // if body is missing, respond with 400 and { message: "missing user data" }
+  // if body is missing name field, respond with 400 and { message: "missing required name field" }
+  next();
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  // validates body on a request to create a new post
+  // if body is missing, respond with 400 and { message: "missing post data" }
+  // if body is missing the text field, respond with 400 and { message: "missing required text field" }
+  next();
 }
 
 module.exports = router;
